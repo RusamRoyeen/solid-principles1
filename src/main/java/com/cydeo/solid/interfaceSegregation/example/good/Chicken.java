ba7f66ddaa316aed1ee1fd9b02233d0810d17ec6
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-22 19:09:29 -0400

    Chicken.java is created

--- src/main/java/com/cydeo/solid/interfaceSegregation/example/good/Chicken.java
+++ src/main/java/com/cydeo/solid/interfaceSegregation/example/good/Chicken.java
@@ -4,12 +4,11 @@ public class Chicken implements CanEat, CanWalk {
 
     @Override
     public void eat() {
-        System.out.println("Eating");
+
     }
 
     @Override
     public void walk() {
-        System.out.println("Walking");
-    }
 
+    }
 }
