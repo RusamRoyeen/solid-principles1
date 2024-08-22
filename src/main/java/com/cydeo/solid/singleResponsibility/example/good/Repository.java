e87077d4c2755f42a7e1d8843e5aaef3015903e8
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-22 17:02:43 -0400

    Repository.java is created

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/Repository.java
@@ -0,0 +1,22 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Repository {
+
+    public static void create(){
+       //Connect to database
+        //Creat  user in database
+    }
+
+    public static void read(){
+        //Connect to database
+        //Creat  user from database
+    }
+    public static void update(){
+        //Connect to database
+        //Creat  user in database
+    }
+    public static void delete(){
+        //Connect to database
+        //Creat  user in database
+    }
+}
