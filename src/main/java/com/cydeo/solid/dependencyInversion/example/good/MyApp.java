35a510be04c2b55a20bd7764c423b4661a066b84
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-22 19:55:55 -0400

    MyApp.java is created

--- src/main/java/com/cydeo/solid/dependencyInversion/example/good/MyApp.java
+++ src/main/java/com/cydeo/solid/dependencyInversion/example/good/MyApp.java
@@ -1,13 +1,11 @@
 package com.cydeo.solid.dependencyInversion.example.good;
 
 public class MyApp {
+    public static void main(String[] args) {
 
-    public static void read(Reader reader) {
+        XMLReader xmlReader = new XMLReader();
 
-        UserReader userReader = new UserReader(reader);
-
+        UserReader userReader = new UserReader(xmlReader);
         userReader.getUsername();
-
     }
-
 }
