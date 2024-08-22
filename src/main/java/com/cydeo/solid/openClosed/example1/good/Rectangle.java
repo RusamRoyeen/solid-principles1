fc99af4ad527bff0d455ce8f4d0f7c98b908213d
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-22 17:24:17 -0400

    Rectangle.java is created

---
+++ src/main/java/com/cydeo/solid/openClosed/example1/good/Rectangle.java
@@ -0,0 +1,12 @@
+package com.cydeo.solid.openClosed.example1.good;
+
+public class Rectangle implements Shape {
+
+    private double width;
+    private double height;
+
+    @Override
+    public void draw() {
+
+    }
+}
