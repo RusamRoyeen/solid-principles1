2f7cab96c8c6608f5009458081add4de23dfbe60
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-22 17:16:11 -0400

    Circle.java is created

--- src/main/java/com/cydeo/solid/openClosed/example1/good/Circle.java
+++ src/main/java/com/cydeo/solid/openClosed/example1/good/Circle.java
@@ -1,12 +1,4 @@
 package com.cydeo.solid.openClosed.example1.good;
 
-public class Circle implements Shape {
-
-    private double radius;
-
-    @Override
-    public void draw() {
-        System.out.println("Drawing Circle.");
-    }
-
+public class Circle {
 }
