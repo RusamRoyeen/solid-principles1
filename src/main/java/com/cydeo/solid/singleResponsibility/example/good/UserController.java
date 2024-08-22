9af1a4fa2d70fafe451984231939af08324f869a
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-21 20:49:25 -0400

    UserController.java is created

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/UserController.java
@@ -0,0 +1,18 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class UserController {
+
+    public static void getRequest(){
+
+        // Get the request
+        // Send this request to validation and security part
+
+    }
+    public static String createResponse(){
+        return "Response";
+    }
+
+    public static void sendResponse(){
+        //Send user a response
+    }
+}
